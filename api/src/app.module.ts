13b3f338c460eab5cfb1ba11c3ed99a20cb6ac50
Dai Nguyen <ngtrdai290197@gmail.com>
2021-10-23 13:02:49 +0700

    Update env

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,9 +7,22 @@ import { TodosModule } from './todos/todos.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      "'mongodb+srv://ngtrdai197:drYHxsYlLiB1nYY5@clusterdemo.mx6zs.mongodb.net/simple-todo-app?retryWrites=true&w=majority'",
-    ),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: (config: ConfigService) => {
+        return {
+          auth: {
+            username: config.get<string>('MONGO_USER'),
+            password: config.get<string>('MONGO_PASSWORD'),
+          },
+          uri: config.get<string>('HOST_MONGODB_URL'),
+        };
+      },
+      inject: [ConfigService],
+    }),
     TodosModule,
   ],
   controllers: [AppController],
